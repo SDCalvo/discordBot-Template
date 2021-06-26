c5fea32a16f67c5dfeeefb5675d8c9f14c57ed93
calvix1 <santiago.d.calvo@gmail.com>
2021-06-26 17:55:20 -0300

    "Leeroy bot start"

---
+++ bot.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+require('dotenv').config();
+const client = new Discord.Client();
+client.login(process.env.BOTTOKEN);
+
+client.on('ready', ()=> {
+
+    console.log('Ready!');
+})
+
+client.on('message', gotMessage);
+
+const replies= ['ed que quiedo jugad denek detadidad',
+        'ed que me nedfeadon ed dompeavances', 
+        'adi decia da guia que lei', 
+        'chango deja que me diviedta'];
+
+function gotMessage(msg){
+
+    console.log(msg.content);
+    if(msg.content === 'damian armate bien'){
+        const index = Math.floor(Math.random() * replies.length);
+        msg.channel.send(replies[index]);
+    }
+}
