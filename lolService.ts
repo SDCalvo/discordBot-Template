706c38045691de5703bf6e2b7f991cdb7c3e8ee7
calvix1 <santiago.d.calvo@gmail.com>
2021-06-27 18:35:42 -0300

    worker file dir: Fixed URLS

--- lolService.ts
+++ lolService.ts
@@ -2,21 +2,22 @@ import axios from "axios";
 import Discord from 'discord.js';
 
 const axiosLoL = axios.create();
-axiosLoL.defaults.baseURL = 'https://la2.api.riotgames.com';
+const la2URL = 'https://la2.api.riotgames.com';
+const americasURL = 'https://americas.api.riotgames.com';
 axiosLoL.defaults.headers['X-Riot-Token'] = process.env.LOL_KEY;
 
 export async function lolApi(msg: Discord.Message) {
   const summoner = msg.content;
 
   //get summoner profile
-  const resSummoner = await axiosLoL.get(`lol/summoner/v4/summoners/by-name/${summoner}`);
+  const resSummoner = await axiosLoL.get(`${la2URL}/lol/summoner/v4/summoners/by-name/${summoner}`);
   //get account ID
   const { puuid } = resSummoner.data;
   console.log("🚀 - lolApi - puuid", puuid);
   //Set ammount of games returned
   const params = { endIndex: 1 }
   //get match history
-  const resMatchs = await axiosLoL.get(`https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=20`);
+  const resMatchs = await axiosLoL.get(`${americasURL}/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=20`);
   console.log("🚀 - lolApi - data", resMatchs.data);
   //get game ID from matches
   const lastMatch = resMatchs.data[0];
